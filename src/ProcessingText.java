f1da16c651ed3b71ad24fe85aa514fbb9e094da1
Ranjeeta <sharma.73@iitj.ac.in>
2022-03-02 19:33:16 +0530

    Changes Committed

--- src/ProcessingText.java
+++ src/ProcessingText.java
@@ -5,7 +5,10 @@ public class ProcessingText extends PApplet{
     public static final int WIDTH = 480;
     public static final int HEIGHT = 640;
     public static final int DIAMETER = 15;
-    public static int ballPosition = 0;
+    int x1 = 0;
+    int x2 = 0;
+    int x3 = 0;
+    int x4 = 0;
 
     public static void main(String[] args){
         PApplet.main("ProcessingText", args);
@@ -23,13 +26,26 @@ public class ProcessingText extends PApplet{
 
     @Override
     public void draw() {
-        for (int speed = 1; speed <= 4; speed++ ){
-            drawBall(speed * ballPosition, speed * HEIGHT / 5);
-        }
-        ballPosition++;
+        createBall1();
+        createBall2();
+        createBall3();
+        createBall4();
     }
 
-    private void drawBall(int position, int height) {
-        ellipse(position,height,DIAMETER, DIAMETER);
+    private void createBall1() {
+        ellipse(x1,HEIGHT / 5,DIAMETER, DIAMETER);
+        x1 = x1 + 1;
+    }
+    private void createBall2() {
+        ellipse(x2,(HEIGHT * 2) / 5,DIAMETER, DIAMETER);
+        x2 = x2 + 2;
+    }
+    private void createBall3() {
+        ellipse(x3,(HEIGHT * 3) / 5,DIAMETER, DIAMETER);
+        x3 = x3 + 3;
+    }
+    private void createBall4() {
+        ellipse(x4,(HEIGHT * 4) / 5,DIAMETER, DIAMETER);
+        x4 = x4 + 4;
     }
  }
